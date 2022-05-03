7ed0bee3370ec190a8995b4c42c32b1e8d982cad
LeticiaAloia <leticia.aloia@gmail.com>
2022-05-03 00:01:52 -0300

    Completo comentarios.

--- src/components/CartIcon/CartIcon.js
+++ src/components/CartIcon/CartIcon.js
@@ -1,6 +1,7 @@
 export default function CartIcon ({counter}) {
     return (
         <div>
+            {/* Imagen en carpeta "images", dentro de "public". */}
             <img src="images/basket_icon.png" alt="Basket widget" />
             {counter}
         </div>
